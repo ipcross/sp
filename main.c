5a2495a3b11c7b00a849371239a3a63c3b894764
cross <cross@vgg.ru>
2013-07-11 20:33:44 +0400

    №52 Счастливый билет

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-long a[3],tmp;
+int a,b[6];
 
 int main(){
   freopen("input.txt","r",stdin);
   freopen("output.txt","w",stdout);
-  scanf("%ld%ld%ld",&a[0],&a[1],&a[2]);
+  scanf("%d",&a);
+	
+  b[0]=a/100000;
+  b[1]=a/10000%10;
+  b[2]=a/1000%10;
+  b[3]=a/100%10;
+  b[4]=a/10%10;
+  b[5]=a%10;
 
-  if(a[2]==a[0]*a[1])printf("%s","YES");
+  if(b[0]+b[1]+b[2]==b[3]+b[4]+b[5])printf("%s","YES");
   else printf("%s","NO");
 
 }
