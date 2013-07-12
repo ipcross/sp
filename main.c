8e7b5ba8c8df8f3007f6c7bd15ddcb6d8d996de6
cross <cross@vgg.ru>
2013-07-12 13:47:27 +0400

    №82 Пересечение множеств

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
-int N, i;
+int N,M,i,a[100000],x;
 
 int main(){
   freopen("input.txt","r",stdin);
   freopen("output.txt","w",stdout);
   
-  scanf("%d",&N);
+  scanf("%d%d",&N,&M);
 
-  int *a = new int[N];
-  
-  for(i=0;i<N;i++) scanf("%d",&a[i]);
-	
-  for(i=N-1;i>=0;i--)printf("%d ",a[i]);
+  for(i=0;i<N;i++)
+  {
+	scanf("%d",&x);
+	a[x]=1;
+  }
+  for(i=0;i<M;i++) 
+  {
+	  scanf("%d",&x);
+	  if(a[x]==1) a[x]=2;
+  }
+  for(i=0;i<100000;i++) 
+	if(a[i]==2)printf("%d ",i);
 
   return 0;
 }
