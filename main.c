81c1b2103c25d2ba6e209fc68120837ee30b81ee
cross <cross@vgg.ru>
2013-07-12 09:14:40 +0400

    №43 Нули

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 
-int N,a=30000,b=0,x;
+char a[100];
+int count=0,sum=0;
 
 int main(){
   freopen("input.txt","r",stdin);
   freopen("output.txt","w",stdout);
   
-  scanf("%d",&N);
+  scanf("%s",&a);
 
-  for(int i=0;i<N;i++)
+  for(int i=0;i<100;i++)
   {
-	  scanf("%d",&x);
-	  if(x<a)a=x;
-	  if(x>b) b=x;
-  }
+	if(a[i]=='0')
+		sum++;
+	else
+	{
+		if(count<sum)count=sum;
+		sum=0;
+	}
+  } 
   
-  printf("%d %d",a,b);
+  printf("%d",count);
 
   return 0;
 }
