383856ad3187e87defe873fc7a60979e9b7aba76
cross <cross@vgg.ru>
2013-07-21 19:24:07 +0400

    №27 Художник

--- main.c
+++ main.c
@@ -1,73 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-long a,b;
-
-long funmax(long num)
-{
-	int n,i,j;
-	char str[10],tmp;
-	if(num<0)num=-num;
-	n=sprintf(str, "%ld", num);
-
-	for (i = 0; i < n-1 ; i++)
-		for (j=n-1;j>i; j--)
-			if(str[j-1]<str[j])
-			{
-				tmp=str[j-1];
-				str[j-1]=str[j];
-				str[j]=tmp;
-			}
-			
-	return atoi(str);
-}
-
-long funmin(long num)
-{
-	int n,i,j;
-	char str[10],tmp;
-	if(num<0)num=-num;
-	n=sprintf(str, "%ld", num);
-		
-	for (i = 0; i < n-1 ; i++)
-		for (j=n-1;j>i; j--)
-			if(str[j-1]>str[j])
-			{
-				tmp=str[j-1];
-				str[j-1]=str[j];
-				str[j]=tmp;
-			}
-			
-	if(str[0]=='0')
-		for(i=1;i<n;i++)
-			if(str[i]!='0')
-			{
-				tmp=str[0];
-				str[0]=str[i];
-				str[i]=tmp;
-				break;
-			}
-		
-	return atoi(str);
-}
 
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
+ 
+	int w,h,n,a[112][112],i,j,x1,y1,x2,y2,c=0,f;
     
-    scanf ("%ld%ld", &a,&b);
+    scanf ("%d%d%d", &w,&h,&n);
 
-	if(a>=0)
+	for(f=0;f<n;f++)
 	{
-		if(b>=0) printf("%ld",funmax(a)-funmin(b));
-		else printf("%ld",funmax(a)+funmax(b));
+		scanf ("%d%d%d%d", &x1,&y1,&x2,&y2);
+		for(i=x1;i<x2;i++)
+			for(j=y1;j<y2;j++)
+				a[i][j]=1;
 	}
-	else
-	{
-		if(b>=0) printf("%ld",-funmin(a)-funmin(b));
-		else printf("%ld",-funmin(a)+funmax(b));
-	}
-
+	
+	for(i=0;i<h;i++)
+		for(j=0;j<w;j++) 
+			if(a[i][j])c++;
+	printf("%d", w*h-c);
+	
     return 0;
 }
