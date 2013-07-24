c2c1986bbcf88f399cc995f204f6a8b8ea3486da
cross <cross@vgg.ru>
2013-07-24 22:01:14 +0400

    №234 Сапер

--- main.c
+++ main.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 #include <algorithm>
 
-int **a, n, m, c, f=0, i, j,l;
+int **a, n, m, k, i,j,l,x,y;
 
 int main(){
 	
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);	
     
-	scanf("%d",&c);
-	for(l=0;l<c;l++){
-		f=0;
-		scanf("%d%d",&n,&m);
-		a = new int*[n];
-		for(i=0;i<n;i++) a[i]=new int[m] ;
+	scanf("%d%d%d",&n,&m,&k);
 
-		for(i=0;i<n;i++)
-			for(j=0;j<m;j++)
-				 scanf("%d",&a[i][j]);
-				 
-		for(i=0;i<n-1;i++){
-			for(j=0;j<m-1;j++){
-				if((a[i][j]+a[i][j+1]+a[i+1][j]+a[i+1][j+1])%4==0)
-				{
-					f=1;
-					break;
-				}
-			}
-			if(f)break;
-		}
-		
-		f?printf("NO\n"):printf("YES\n");
-		
-		delete a;
+	a = new int*[n+2];
+	for(i=0;i<n+2;i++) a[i]=new int[m+2] ;
+
+//	for(i=0;i<n+2;i++)
+//		for(j=0;j<m+2;j++) a[i][j]=0;
+
+	for(l=0;l<k;l++){
+		scanf("%d%d",&x,&y);
+		if(a[x][y]!=9)
+		{
+			a[x][y]=9;
+			if(a[x-1][y-1]!=9)a[x-1][y-1]++;
+			if(a[x-1][y]!=9)a[x-1][y]++;
+			if(a[x-1][y+1]!=9)a[x-1][y+1]++;
+			if(a[x][y-1]!=9)a[x][y-1]++;
+			if(a[x][y+1]!=9)a[x][y+1]++;
+			if(a[x+1][y-1]!=9)a[x+1][y-1]++;
+			if(a[x+1][y]!=9)a[x+1][y]++;
+			if(a[x+1][y+1]!=9)a[x+1][y+1]++;
+		}	
 	}	
+	
+	for(i=1;i<n+1;i++){
+		for(j=1;j<m+1;j++){
+			if(a[i][j]==0)printf(".");
+			else { if(a[i][j]==9)printf("*");
+			else printf("%d",a[i][j]);}
+		}
+		if(i!=n)printf("\n");
+	}	
+	
 }
