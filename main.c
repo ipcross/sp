682ba26292c3aa20896f47829b0e13d68af58310
cross <cross@vgg.ru>
2013-07-28 19:16:36 +0400

    №22 Единицы

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int a[120][120], n, i=1, j, k, p;
+long n;
 
 int main(){
 	
@@ -8,59 +8,14 @@ int main(){
     freopen("output.txt", "w", stdout);	
     
 	scanf("%d",&n);
-	int i=1,j=1,k=1;
-	a[i][j] = k;
-
-	if(n==1){
-		printf("%d",1);
-		return 0;
-	}
-
-	while(1){
-    if (i < n){  //{вниз}
-		i++;
-		k++;
-		a[i][j]= k;
-	}
-	else { 		// {вправо}
-		j++; 
-		k++;
-		a[i][j]= k;
-	}
+	int i,l=0,count=0;
 	
-	while(i > 1 && j < n) {
-		j++;  //{диагональ вверх-лево}
-		i--;
-		k++;
-		a[i][j] = k;
-	}
+	do{
+		if(n%2)count++;
+		n=n/2;
+	}while(n>0);
 
-	if (j < n){
-		j++; //{вправо}
-		k++;
-		a[i][j] = k;
-	}
-	else
-		if (i < n){
-			i++; //{вниз}
-			k++;
-			a[i][j] = k;
-		}
-	while (j > 1 && i < n){
-		i++; //{диагональ влево-вниз}
-		j--;
-		k++;
-		a[i][j] = k;
-	}
-	
-	if( i == n && j == n) break;//{конец правый нижний угол}
-	}	
-	
-	for(i=1; i<=n; i++)
-		for(j=1; j<=n; j++){
-			printf("%6d",a[i][j]);
-			if(j==n) printf("\n");
-		}		
+	printf("%d", count);
 	return 0;
 }
 
