aa5544fec77b4781c9f9489a3d3a4454a7e320c6
cross <cross@vgg.ru>
2013-07-28 20:04:00 +0400

    №315 Наименьшая система счисления

--- main.c
+++ main.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
-long n,k;
+char s[300];
 
 int main(){
 	
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);	
     
-	scanf("%ld%ld",&n,&k);
-	int i,p=1,s=0;
+	gets(s);
+	int i,l=-1;
 	
-	do{
-		p=p*(n%k);
-		s=s+(n%k);
-		n=n/k;
-	}while(n>0);
-
-	printf("%d", p-s);
+	for(i=0;i<strlen(s);i++)
+	{
+		if(s[i]>=48 && s[i]<=57){
+			if(s[i]-47>l)l=s[i]-47;
+		}
+		else {
+			if(s[i]>=65 && s[i]<=90){
+				if(s[i]-54>l)l=s[i]-54;
+			}
+			else{
+				printf("%d",-1);
+				return 0;
+			}
+		}
+	}
+	
+	if(l==1)l++;
+	printf("%d", l);
 	return 0;
 }
 
