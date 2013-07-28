c44d7ef51276c0c575d720eb0787dbb4d8350a23
cross <cross@vgg.ru>
2013-07-28 19:26:05 +0400

    №59 Несложное вычисление

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-long n;
+long n,k;
 
 int main(){
 	
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);	
     
-	scanf("%d",&n);
-	int i,l=0,count=0;
+	scanf("%ld%ld",&n,&k);
+	int i,p=1,s=0;
 	
 	do{
-		if(n%2)count++;
-		n=n/2;
+		p=p*(n%k);
+		s=s+(n%k);
+		n=n/k;
 	}while(n>0);
 
-	printf("%d", count);
+	printf("%d", p-s);
 	return 0;
 }
 
