d4879b6a96f5976d275440606bae600ba711220f
cross <cross@vgg.ru>
2013-07-30 15:01:38 +0400

    №275 Делимость на 7

--- main.c
+++ main.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-long m,l;
+int n;
+char s[1100],t[1010];
 
 int main(){
 	
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);	
 
- 	scanf("%d",&m);
-	int i,flag=0,t;
+ 	scanf("%d",&n);
+	int i,j,sum=0;
 	
-	do{
+	for(i=0;i<n;i++)
+	{
+		sum=0;
+		scanf("%s",&s);
 		
-		t=m%2;
-		if(flag==0&&t) flag=1;
-		if(flag) l=2*l+t;
-		m=m/2;
-	}while(m>0);   
-
-	printf("%ld", l);
+		while(strlen(s)%3){
+			t[0]='0';
+			t[1]='\0';
+			strcat(t,s);
+			strcpy(s,t);
+		}
+		
+		for(j=0;j<strlen(s)/3;j++)
+			sum=sum+s[3*j]*4+s[3*j+1]*2+s[3*j+2];
+		
+		if(sum%7)printf("No\n");
+		else printf("Yes\n");
+	}
+	
 	return 0;
 }
 
