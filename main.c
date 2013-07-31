cd808a60b4e695b6422dea88ffc9049573fda552
cross <cross@vgg.ru>
2013-07-31 10:27:44 +0400

    №208 Забавная игра

--- main.c
+++ main.c
@@ -1,41 +1,21 @@
 #include <stdio.h>
-#include <string.h>
-
-int n;
-char s[1100],t[1010];
-
+#include <math.h>
 int main(){
-	
+    int i,max,n,p=0;
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);	
-
  	scanf("%d",&n);
-	int i,j,sum=0;
-	
-	for(i=0;i<n;i++)
-	{
-		sum=0;
-		scanf("%s",&s);
-		
-		while(strlen(s)%3){
-			t[0]='0';
-			t[1]='\0';
-			strcat(t,s);
-			strcpy(s,t);
-		}
-		
-		for(j=0;j<strlen(s)/3;j++)
-			sum=sum+s[3*j]*4+s[3*j+1]*2+s[3*j+2];
-		
-		if(sum%7)printf("No\n");
-		else printf("Yes\n");
-	}
-	
+	max=n;
+	p=log2(n)+1;
+	for(i=0;i<p-1;i++){
+		n=n<<1;
+		if(n/(1<<p))n=n&((1<<p)-1)|1;
+		else n=n&((1<<p)-1);
+		if(max<n)max=n;}
+	printf("%d",max);
 	return 0;
 }
 
 
  
  
-
-
