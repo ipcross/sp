deaf1e46efa7e564e5d0074e3a83eae80053f3ce
cross <cross@vgg.ru>
2013-07-31 23:03:40 +0400

    №578 Система счисления(TLE)

--- main.c
+++ main.c
@@ -1,55 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 
-long n,tmp;
-
-char simvol(int num)
-{
-	if(num<10)return char(48+num);
-	if(num>9)return char(55+num); 
-}
+long n;
 
 int main(){
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);	
-
-	int i,j,m[40],p=0,l;
-	char s[1000];
-	
+	int i,m[100],p=0,tmp;
  	scanf("%ld",&n);
-	
-	for(i=2;i<=36;i++){
-		l=-1;
-		tmp=n;
-		do{
-			l++;
-			s[l]=simvol(tmp%i);
-			tmp=tmp/i;
-		}while(tmp>0);
-		l++;
-		s[l]='\0';
-	
-		if(l==1){
-			m[p]=i;
-			p++;		
-		}
-	
-		for(j=0;j<l/2;j++){
-			if(s[j]!=s[l-1-j])break;
-			if(j==l/2-1){
-				m[p]=i;
-				p++;
+	do{
+		m[p]=n%3;
+		p++;
+		n=n/3;
+	}while(n>0);
+	for(i=0;i<p/2;i++){
+		tmp=m[i];
+		m[i]=m[p-1-i];
+		m[p-1-i]=tmp;
+	}
+	i=1;
+	while(1){
+		if(m[i]==0){
+			if(m[i-1]==1){
+				m[i-1]=0;
+				m[i]=3;
+				if(i>1)i=i-2;
 			}
+			if(m[i-1]==2){
+				m[i-1]=1;
+				m[i]=3;
+			}
+			if(m[i-1]==3){
+				m[i-1]=2;
+				m[i]=3;
+			}
+			i++;
 		}
+		else i++;	
+		if(i==p-1 && m[i]!=0)break;
 	}
-	
-	if(p==0) printf("none");
-	else {
-		if(p==1) printf("unique\n%d",m[0]);
-		else{
-			printf("multiple\n");
-			for(i=0;i<p;i++) printf("%d ",m[i]);
-		}
-	}
+	for(i=0;i<p;i++) 
+		if(m[i])printf("%d",m[i]);	
 	return 0;
 }
